199930cf3c13384544fc7d392e604f137df01fe4
nyeliep <gielpalnyeliep@gmail.com>
2023-04-14 22:05:06 +0300

    commit

---
+++ math_.py
@@ -0,0 +1,31 @@
+def add(a,b):
+    answer=(a+b)
+    return answer
+
+def subtract(a,b):
+    answer=(a-b)
+    return answer
+
+def divide(a,b):
+    answer=(a/b)
+    return answer
+
+def multiply(a,b):
+    answer=(a*b)
+    return answer
+
+def remainder(a,b):
+     answer=(a%b)
+     return answer
+
+def sum(*numbers)
+    answer = 0
+    for number in numbers:
+        answer += number
+    return answer
+
+def multiply(*numbers)
+    answer = 1
+    for number in numbers
+        answer *= number
+    return answer
